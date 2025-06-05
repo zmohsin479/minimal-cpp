2a72b1e93959e235f49754cacbd49f34772a58f6
Zain Mohsin <zmohsin479@student.fuhsd.org>
2025-06-05 18:16:01 +0000

    Made user-input calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
+using namespace std;
+
 #include <iostream>
 
-int main() {
-    std::cout << "hello world" << std::endl;
-    return 0;
+class myClass
+{
+public:
+    int x;
+    int y;
+    int sum;
+    int multiply;
+    void show()
+    {
+        cout << endl << "Multiplying these two numbers gets you: " << multiply << endl;
+
+        cout << "The sum of these two numbers is is: " << sum << endl << endl;
+    }
+};
+
+int main()
+{
+    myClass object;
+    cout << "Type a number: ";
+    cin >> object.x;
+    cout << "Type another number: ";
+    cin >> object.y;
+    object.sum = object.x + object.y;
+    object.multiply = object.x * object.y;
+    object.show();
 }
